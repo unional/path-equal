d4bde7575cbf264dca4eeb3bcbfe47ba79457bea
Homa Wong <homawong@gmail.com>
2018-05-02 15:05:46 -0700

    feat: support drive letter, '.' and '..' (#5)
    * feat: support drive letter, '.' and '..'
    
    * update devDeps
    
    * chore(build): pin to node 8.0.0
    
    * misc

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,22 +1,40 @@
+import t from 'assert'
 
 import { pathEqual } from '.'
 
 test('path with same style passes', () => {
-  expect(pathEqual('a', 'a')).toBe(true)
-  expect(pathEqual('a/b', 'a/b')).toBe(true)
-  expect(pathEqual('a\\b', 'a\\b')).toBe(true)
+  t(pathEqual('a', 'a'))
+  t(pathEqual('a/b', 'a/b'))
+  t(pathEqual('a\\b', 'a\\b'))
 })
 
 test('same style different value throws', () => {
-  expect(pathEqual('a', 'b')).toBe(false)
+  t.equal(pathEqual('a', 'b'), false)
 })
 
 test('UNIX style matches Windows style', () => {
-  expect(pathEqual('a/b', 'a\\b')).toBe(true)
-  expect(pathEqual('a/b/c', 'a\\b\\c')).toBe(true)
-  expect(pathEqual('a\\b', 'a/b')).toBe(true)
-  expect(pathEqual('a\\b\\c', 'a/b/c')).toBe(true)
+  t(pathEqual('a/b', 'a\\b'))
+  t(pathEqual('a/b/c', 'a\\b\\c'))
+  t(pathEqual('a\\b', 'a/b'))
+  t(pathEqual('a\\b\\c', 'a/b/c'))
 })
 
-// not supported yet
-test(`/d/foo should match d:\\foo`)
+test(`/d/foo should match d:\\foo`, () => {
+  expect(pathEqual('/d/foo', 'd:\\foo')).toBe(true)
+})
+
+test(`with '.' (current dir)`, () => {
+  t(pathEqual('./a', 'a'))
+  t(pathEqual('a/b/.', 'a/b'))
+  t(pathEqual('a/b/./', 'a/b'))
+  t(pathEqual('a/b/./c', 'a/b/c'))
+})
+
+test(`with '..' (parent dir)`, () => {
+  t(pathEqual('a/../b', 'b'))
+  t(pathEqual('a/b/../', 'a'))
+  t(pathEqual('a/b/..', 'a'))
+  t(pathEqual('a/b/../c/../d', 'a/d'))
+  t(pathEqual('../a', '../a'))
+  t(pathEqual('../a/b/..', '../a'))
+})
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 export function pathEqual(actual: string, expected: string) {
   if (actual === expected) return true
 
-  const normalizedActual = actual.replace(/\\/g, '/')
-  const normalizedExpected = expected.replace(/\\/g, '/')
+  const normalizedActual = normalizePath(actual)
+  const normalizedExpected = normalizePath(expected)
   return normalizedActual === normalizedExpected
+}
+
+function normalizePath(value: string) {
+  return value.replace(/\\/g, '/')
+    .replace(/(\w):/, '/$1')
+    .replace(/(\w)\/\.\.\/?/g, '')
+    .replace(/^\.\//, '')
+    .replace(/\/\.\//, '/')
+    .replace(/\/\.$/, '')
+    .replace(/\/$/, '')
 }

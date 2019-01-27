1679bb70dc17b24d603bdc6a6824becfbab92d70
Homa Wong (unional) <homawong@gmail.com>
2019-01-26 19:49:56 -0800

    chore: update project

--- scripts/watch.js
+++ scripts/watch.js
@@ -7,16 +7,12 @@ cp.spawn('tsc', ['-w'], { shell: true })
   .stdout.on('data', (data) => {
     const text = data.toString()
     process.stdout.write(text)
-    if (/.*Compilation complete/.test(text)) {
+    if (/.*Found 0 errors/.test(text)) {
       if (!runner) {
         runner = cp.spawn('jest', ['--watch'], {
           stdio: 'inherit',
           shell: true
         })
       }
-      cp.spawnSync('npm', ['run', 'lint'], {
-        stdio: 'inherit',
-        shell: true
-      })
     }
   })

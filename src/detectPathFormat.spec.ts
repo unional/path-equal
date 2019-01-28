8573f4a96e1f1ba38ace39d4c7de6aeeddba92f0
Homa Wong (unional) <homawong@gmail.com>
2019-01-27 21:12:43 -0800

    remove window test

--- src/detectPathFormat.spec.ts
+++ src/detectPathFormat.spec.ts
@@ -4,8 +4,3 @@ import { detectPathFormat } from './detectPathFormat';
 test('detects UNC format', () => {
   t.strictEqual(detectPathFormat('\\\\server\\drive\\some-path\\x.txt'), 'unc')
 })
-
-test('detects Windows path', () => {
-  t.strictEqual(detectPathFormat('C:\\a\\x.txt'), 'windows')
-  t.strictEqual(detectPathFormat('a\\x.txt'), 'windows')
-})
--- src/detectPathFormat.ts
+++ src/detectPathFormat.ts
@@ -1,5 +1,3 @@
 export function detectPathFormat(pathString: string) {
   if (pathString.startsWith('\\\\')) return 'unc'
-
-  if (/^\w\:/.test(pathString)) return 'windows'
 }

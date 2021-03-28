7305b757582055294d01b30cc1eaf8baee1f2686
Homa Wong <homawong@gmail.com>
2021-03-28 00:33:53 -0700

    Update jest.config.js
    remove semi-colons

--- jest.config.js
+++ jest.config.js
@@ -1,2 +1,2 @@
-const common = require('@unional/devpkg-node/simple/config/jest.common');
-module.exports = Object.assign(common, {});
+const common = require('@unional/devpkg-node/simple/config/jest.common')
+module.exports = Object.assign(common, {})

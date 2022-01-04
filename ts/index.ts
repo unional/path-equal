de847c9eaf6393ae0cb9c4b178f45873708392e0
Homa Wong <homawong@gmail.com>
2022-01-03 22:54:20 -0800

    fix: reduce size

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,23 @@
-export * from './pathEqual';
+export function pathEqual(actual: string, expected: string): boolean {
+  return (actual === expected) || (normalizePath(actual) === normalizePath(expected))
+}
+
+function normalizePath(path: string): string {
+  const replace: [RegExp, string][] = [
+    [/\\/g, '/'],
+    [/(\w):/, '/$1'],
+    [/(\w+)\/\.\.\/?/g, ''],
+    [/^\.\//, ''],
+    [/\/\.\//, '/'],
+    [/\/\.$/, ''],
+    [/\/$/, ''],
+  ];
+
+  replace.forEach(array => {
+    while (array[0].test(path)) {
+      path = path.replace(array[0], array[1])
+    }
+  })
+
+  return path
+}

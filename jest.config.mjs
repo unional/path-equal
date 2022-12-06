7d70a4890c0ab0d3549bd0c8e91265a61426b214
unional <homawong@gmail.com>
2022-12-06 01:49:32 -0800

    chore: update jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,28 +1,4 @@
+/** @type {import('jest').Config} */
 export default {
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  collectCoverageFrom: [
-    '<rootDir>/ts/**/*.[jt]s'
-  ],
-  roots: ['<rootDir>/ts'],
-  testMatch: ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
-  transform: {
-    '^.+\\.m?[t]sx?$': ['ts-jest', {
-      isolatedModules: true,
-      useESM: true
-    }],
-  },
-  watchPlugins: [
-    'jest-watch-suspend',
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-    [
-      'jest-watch-toggle-config', { 'setting': 'verbose' },
-    ],
-    [
-      'jest-watch-toggle-config', { 'setting': 'collectCoverage' },
-    ],
-  ],
+  preset: '@repobuddy/jest/presets/ts-esm'
 }
